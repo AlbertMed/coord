f3961fb8b8f2ca50ad9dd6d22f36a49877cad1b5
Albert M <alberto_me_d@hotmail.com>
2014-12-10 04:26:50 +0000

    se agregaron los archivos de la pantalla principal

---
+++ includes/styles.php
@@ -0,0 +1,4 @@
+<link rel="shortcut icon" href="favicon.ico">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="lib/FontAwesome/css/font-awesome.min.css">
+<link href='http://fonts.googleapis.com/css?family=Handlee' rel='stylesheet' type='text/css'>
